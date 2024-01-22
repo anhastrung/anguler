daedcb1c45cc05bc50e18f15fa949972102af779
Nguyễn Tuấn Anh <137992181+anhastrung@users.noreply.github.com>
2024-01-22 09:45:45 +0700

    in ra chi tiet product

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { IProduct } from 'src/interfaces/IProduct';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-
+  @Input() id: number = 0
+  product: IProduct | null = null
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    if (this.id > 0) {
+      this.http.get("http://localhost:3000/products/" + this.id).subscribe(data => this.product = data)
+    }
+    else {
+      location.replace("/product")
+    }
+  }
 }
