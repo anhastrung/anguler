2a57d7fb07521bf1b56d1d65e256e823498795c1
Nguyễn Tuấn Anh <137992181+anhastrung@users.noreply.github.com>
2024-01-22 09:46:29 +0700

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,11 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 const routes: Routes = [
-
+  { path: '', component: HomePageComponent },
+  { path: 'product', component: ProductListComponent },
+  { path: 'product/:id', component: ProductComponent },
+  { path: 'home', redirectTo:"/", pathMatch:"full" },
+  { path: '**', component: NotFoundPageComponent },
 ]
 @NgModule({
   declarations: [
@@ -20,6 +24,7 @@ const routes: Routes = [
     ProductComponent,
     ProductListComponent
   ],
+  
   imports: [
     BrowserModule,
     HttpClientModule,
